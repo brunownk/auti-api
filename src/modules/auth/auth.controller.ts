8059cddb12aa3f2a3ca93c819d8a7842009c5853
Bruno Neckel <neckel.bw@gmail.com>
2023-10-11 02:12:51 -0300

    chore: added auth module and changed user module

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin';
+import { isPublic } from 'src/shared/decorators/IsPublic';
+
+@isPublic()
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  signin(@Body() signDto: SigninDto) {
+    return this.authService.signin(signDto);
+  }
+
+  @Post('signup')
+  signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+}
---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { env } from 'src/shared/config/env';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '7d' },
+      secret: env.jwtSecret,
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
